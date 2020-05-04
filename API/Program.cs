b7cd3c0e8c36c4f6d52274a5e936f92300564654
Pycnonotus <anton.golo.fh@gmail.com>
2020-05-04 10:46:44 +0300

    first comit

---
+++ API/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API {
+    public class Program {
+        public static void Main (string[] args) {
+            var host = CreateHostBuilder (args).Build ();
+            using (var scope = host.Services.CreateScope ()) {
+                var servise = scope.ServiceProvider;
+
+                try {
+                    var context = servise.GetRequiredService<DataContext> ();
+                    context.Database.Migrate ();
+                } catch (Exception ex) {
+                    var logger = servise.GetRequiredService<ILogger<Program>> ();
+                    logger.LogError (ex, "An Error occured druring migration");
+                }
+
+            }
+
+            host.Run ();
+        }
+
+        public static IHostBuilder CreateHostBuilder (string[] args) =>
+            Host.CreateDefaultBuilder (args)
+            .ConfigureWebHostDefaults (webBuilder => {
+                webBuilder.UseStartup<Startup> ();
+            });
+    }
+}
