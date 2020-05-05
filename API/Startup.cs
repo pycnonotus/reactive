6cca0b8503151c8a776f8a1a464c3c8d38498cef
Pycnonotus <anton.golo.fh@gmail.com>
2020-05-05 13:00:00 +0300

    Added a few depndecy to the clint side

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,37 +13,51 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
-namespace API {
-    public class Startup {
-        public Startup (IConfiguration configuration) {
+namespace API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices (IServiceCollection services) {
-            services.AddDbContext<DataContext> (opt => {
-                opt.UseSqlite (Configuration.GetConnectionString ("DefaultConnection"));
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<DataContext>(opt =>
+            {
+                opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
-
-            services.AddControllers ();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CrosPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment ()) {
-                app.UseDeveloperExceptionPage ();
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
             }
 
             // app.UseHttpsRedirection();
+            app.UseCors("CrosPolicy");
+            app.UseRouting();
 
-            app.UseRouting ();
+            app.UseAuthorization();
 
-            app.UseAuthorization ();
-
-            app.UseEndpoints (endpoints => {
-                endpoints.MapControllers ();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
             });
         }
     }
