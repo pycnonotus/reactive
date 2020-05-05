bd7271db6d84cf53fa302117b0af630bcee098ab
Pycnonotus <anton.golo.fh@gmail.com>
2020-05-05 23:55:39 +0300

    Created the API for Activites

--- API/Program.cs
+++ API/Program.cs
@@ -10,30 +10,39 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Persistence;
 
-namespace API {
-    public class Program {
-        public static void Main (string[] args) {
-            var host = CreateHostBuilder (args).Build ();
-            using (var scope = host.Services.CreateScope ()) {
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
                 var servise = scope.ServiceProvider;
 
-                try {
-                    var context = servise.GetRequiredService<DataContext> ();
-                    context.Database.Migrate ();
-                } catch (Exception ex) {
-                    var logger = servise.GetRequiredService<ILogger<Program>> ();
-                    logger.LogError (ex, "An Error occurred during migration");
+                try
+                {
+                    var context = servise.GetRequiredService<DataContext>();
+                    context.Database.Migrate();
+                    Seed.SeedData(context);
+                }
+                catch (Exception ex)
+                {
+                    var logger = servise.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An Error occurred during migration");
                 }
 
             }
 
-            host.Run ();
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder (string[] args) =>
-            Host.CreateDefaultBuilder (args)
-            .ConfigureWebHostDefaults (webBuilder => {
-                webBuilder.UseStartup<Startup> ();
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
             });
     }
 }
