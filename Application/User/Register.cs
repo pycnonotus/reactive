7c0b34de929e93384249e648626b0c0a3f30d0c9
Pycnonotus <anton.golo.fh@gmail.com>
2020-05-13 19:09:28 +0300

    Added login to the API

---
+++ Application/User/Register.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Errors;
+using Application.Interfaces;
+using Application.Validators;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.User
+{
+    public class Register
+    {
+        public class Command : IRequest<User>
+        {
+            public string DisplayName { get; set; }
+            public string Username { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.DisplayName).NotEmpty();
+                RuleFor(x => x.Username).NotEmpty();
+                RuleFor(x => x.Email).NotEmpty().EmailAddress();
+                RuleFor(x => x.Password).Password();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, User>
+        {
+            private readonly DataContext _context;
+            private readonly UserManager<AppUser> _userManager;
+            private readonly IJwtGenerator _jWTGenerator;
+            public Handler(DataContext context, UserManager<AppUser> userManager, IJwtGenerator jWTGenerator)
+            {
+                this._jWTGenerator = jWTGenerator;
+                this._userManager = userManager;
+                this._context = context;
+            }
+
+            public async Task<User> Handle(Command request, CancellationToken cancellationToken)
+            {
+
+                if (await _context.Users.Where(x => x.Email == request.Email).AnyAsync())
+                {
+                    throw new RestException(HttpStatusCode.BadRequest, new
+                    {
+                        Email = " Email already exists"
+                    });
+                }
+                if (await _context.Users.Where(x => x.UserName == request.Username).AnyAsync())
+                {
+                    throw new RestException(HttpStatusCode.BadRequest, new
+                    {
+                        User = "User already exists"
+                    });
+                }
+                var user = new AppUser()
+                {
+                    DisplayName = request.DisplayName,
+                    Email = request.Email,
+                    UserName = request.Username
+                };
+
+
+                var success = await _userManager.CreateAsync(user, request.Password);
+
+                if (success.Succeeded)
+                {
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = _jWTGenerator.CreateToken(user),
+                        Username = user.UserName,
+                        Image = null
+                    };
+                }
+                throw new Exception("Problem creating new user");
+            }
+        }
+    }
+}
--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,14 +1,42 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static void SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManger)
         {
+
+            if (!userManger.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser{
+                        DisplayName = "Anton",
+                        UserName = "anton",
+                        Email = "An@ton.co.il"
+                    },
+                     new AppUser{
+                        DisplayName = "Tom",
+                        UserName = "tom",
+                        Email = "tom@ton.co.il"
+                    }, new AppUser{
+                        DisplayName = "Alex",
+                        UserName = "alex",
+                        Email = "Alex@ton.co.il"
+                    }
+                };
+                foreach (var user in users)
+                {
+                    await userManger.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+
             if (!context.Activities.Any())
             {
                 var activities = new List<Activity>
